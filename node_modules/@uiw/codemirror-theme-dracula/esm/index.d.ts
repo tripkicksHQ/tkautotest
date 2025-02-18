756825c537f3e100cd1a1c9052d601f264b8795b
tripkicksHQ <sales@tripkicks.com>
2025-02-18 02:12:04 -0600

    Updated banner logic for tiles.And maybe added dracula theme support.

---
+++ node_modules/@uiw/codemirror-theme-dracula/esm/index.d.ts
@@ -0,0 +1,5 @@
+import { type CreateThemeOptions } from '@uiw/codemirror-themes';
+export declare const defaultSettingsDracula: CreateThemeOptions['settings'];
+export declare const draculaDarkStyle: CreateThemeOptions['styles'];
+export declare const draculaInit: (options?: Partial<CreateThemeOptions>) => import("@codemirror/state").Extension;
+export declare const dracula: import("@codemirror/state").Extension;
