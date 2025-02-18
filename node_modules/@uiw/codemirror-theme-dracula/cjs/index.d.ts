94907909712c966e7bc678e9b42a2749f821f7c6
tripkicksHQ <sales@tripkicks.com>
2025-02-18 02:20:26 -0600

    updated codemirror to 5

--- node_modules/@uiw/codemirror-theme-dracula/cjs/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { type CreateThemeOptions } from '@uiw/codemirror-themes';
-export declare const defaultSettingsDracula: CreateThemeOptions['settings'];
-export declare const draculaDarkStyle: CreateThemeOptions['styles'];
-export declare const draculaInit: (options?: Partial<CreateThemeOptions>) => import("@codemirror/state").Extension;
-export declare const dracula: import("@codemirror/state").Extension;
